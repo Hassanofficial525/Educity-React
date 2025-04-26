96516499be9e1e664a16ac91bf72b83bc9f031ca
Hassanofficial525 <hassanofficial525@gmail.com>
2025-04-26 19:06:17 +0500

    Added Contact section

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -1,17 +1,21 @@
 import React from 'react'
 
 import './Contact.css'
+import msg_icon from '../../assets/msg-icon.png'
 
+import mail_icon from '../../assets/mail-icon.png'
+import phone_icon from '../../assets/phone-icon.png'
+import location_icon from '../../assets/location-icon.png'
 const Contact = () => {
   return (
     <div className='contact'>
         <div className='contact-col'>
-            <h3>Send us a Message</h3>
-            <p>Feel free to reach out to us</p>
+            <h3>Send us a Message<img src={msg_icon} ></img></h3>
+            <p>Feel free to reach out to us through contact form or find our Contact information below. Your feedback, questions, and suggestions are important to us as we strive to provide exceptional service to our university community.</p>
             <ul>
-                <li></li>
-                <li></li>
-                <li></li>
+                <li><img src={mail_icon} alt=''></img>Contact@GreateStack.dev</li>
+                <li><img src={phone_icon} alt=''></img>+1 123-456--7890</li>
+                <li><img src={location_icon} alt=''></img>77 Massachussets Ave, Cambridge<br></br> MA 02139, United States</li>
             </ul>
         </div>
     </div>
