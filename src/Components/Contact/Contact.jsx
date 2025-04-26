e97e4ece3b0b81b1fc5a3a0d572e9ee40f4e08cb
Hassanofficial525 <hassanofficial525@gmail.com>
2025-04-26 14:35:16 +0500

    Added Slider and other sections.md

---
+++ src/Components/Contact/Contact.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+import './Contact.css'
+
+const Contact = () => {
+  return (
+    <div className='contact'>
+        <div className='contact-col'>
+            <h3>Send us a Message</h3>
+            <p>Feel free to reach out to us</p>
+            <ul>
+                <li></li>
+                <li></li>
+                <li></li>
+            </ul>
+        </div>
+    </div>
+  )
+}
+
+export default Contact
